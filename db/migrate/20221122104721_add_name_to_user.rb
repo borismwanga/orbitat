6017e8203b31a6c0584f3249f0379cdd4379a885
Kacper Chmielewski <kchmielewski@tlen.pl>
2022-11-22 11:48:11 +0100

    Fields added to the Users db

---
+++ db/migrate/20221122104721_add_name_to_user.rb
@@ -0,0 +1,8 @@
+class AddNameToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :space_driving_license, :string
+  end
+end
