0e8742fa28efcfa6c4a7388bc13e8a093afb03b9
Kacper Chmielewski <kchmielewski@tlen.pl>
2022-11-22 14:10:39 +0100

    User Controller added + user view: show & update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, status: :see_other
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :phone_number, :space_driving_license)
+  end
+
 end
