33d5fe93d9220468e0cb14c5891abcd66115f592
Xeniouska <115425494+Xeniouska@users.noreply.github.com>
2022-11-23 16:06:37 +0100

    correction of folder, path of the reservation form plus added @spaceship as parameter in the form

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -2,15 +2,16 @@ class SpaceshipsController < ApplicationController
     def index
         @spaceships = Spaceship.all
       end
-    
+
       def show
         @spaceship = Spaceship.find(params[:id])
+        @reservation = Reservation.new
       end
-    
+
       def new
         @spaceship = Spaceship.new
       end
-    
+
       def create
         @spaceship = Spaceship.new(spaceship_params)
         if @spaceship.save
@@ -19,11 +20,11 @@ class SpaceshipsController < ApplicationController
           render :new
         end
       end
-    
+
       def edit
         @spaceship = Spaceship.find(params[:id])
       end
-    
+
       def update
         @spaceship = Spaceship.find(params[:id])
         if @spaceship.update(spaceship_params)
@@ -32,12 +33,12 @@ class SpaceshipsController < ApplicationController
           render :new
         end
       end
-    
+
       def destroy
       end
-    
+
       private
-    
+
       def spaceship_params
         params.require(:spaceship).permit(:name, :category, :location, :price, :description, :speed_of_light, :capacity, :fuel_drive, :brand)
       end
