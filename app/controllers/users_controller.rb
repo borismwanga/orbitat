7deca8de7768426dd96ea2296486f2889ee5f68c
Boris Mwnaga <borismwanga@gmail.com>
2022-11-23 14:55:17 +0100

    working on it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def update
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def user_params
