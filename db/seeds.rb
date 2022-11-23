a3d0d9e3ec1adb2eda8ca0de39c3aeefb2101c46
Boris Mwnaga <borismwanga@gmail.com>
2022-11-23 08:12:23 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+
+# user
+
+125.times {
+    rand = SecureRandom.alphanumeric
+    User.create(first_name: Faker::Name.first_name  , last_name: Faker::Name.last_name , email:Faker::Internet.email, phone_number:Faker::PhoneNumber.cell_phone, space_driving_license: rand, password:"123456",)
+}
+
+25.times{
+    capacity = rand(10000)
+    randCategroy = ('A'..'E').to_a.sample
+    speed = [true, false].sample
+    user = (5..45).to_a.sample
+    sp = Spaceship.create(name:Faker::Movies::StarWars.vehicle, category: randCategroy, location: Faker::Movies::StarWars.planet ,price:Faker::Number.between(from: 200.0, to: 10000.0) ,description: Faker::Movies::StarWars.quote, speed_of_light: speed, capacity: capacity, fuel_drive: Faker::Science.element_state, brand: Faker::Space.company, user_id: user)
+}
