a78b2f08b62c6a8227fa7cfecda8f375da936a99
Kacper Chmielewski <kchmielewski@tlen.pl>
2022-11-24 13:57:59 +0100

    Profile view ready: route for my_ships, links to change profile data

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,45 +1,49 @@
 class SpaceshipsController < ApplicationController
-    def index
-        @spaceships = Spaceship.all
-      end
+  def index
+    @spaceships = Spaceship.all
+  end
 
-      def show
-        @spaceship = Spaceship.find(params[:id])
-        @reservation = Reservation.new
-      end
+  def show
+    @spaceship = Spaceship.find(params[:id])
+    @reservation = Reservation.new
+  end
 
-      def new
-        @spaceship = Spaceship.new
-      end
+  def new
+    @spaceship = Spaceship.new
+  end
 
-      def create
-        @spaceship = Spaceship.new(spaceship_params)
-        if @spaceship.save
-          redirect_to spaceship_path(@spaceship)
-        else
-          render :new
-        end
-      end
+  def my_spaceships
+    @spaceships = Spaceship.all.where(user: current_user)
+  end
 
-      def edit
-        @spaceship = Spaceship.find(params[:id])
-      end
+  def create
+    @spaceship = Spaceship.new(spaceship_params)
+    if @spaceship.save
+      redirect_to spaceship_path(@spaceship)
+    else
+      render :new
+    end
+  end
 
-      def update
-        @spaceship = Spaceship.find(params[:id])
-        if @spaceship.update(spaceship_params)
-          redirect_to spaceship_path(@spaceship)
-        else
-          render :new
-        end
-      end
+  def edit
+    @spaceship = Spaceship.find(params[:id])
+  end
 
-      def destroy
-      end
+  def update
+    @spaceship = Spaceship.find(params[:id])
+    if @spaceship.update(spaceship_params)
+      redirect_to spaceship_path(@spaceship)
+    else
+      render :new
+    end
+  end
 
-      private
+  def destroy
+  end
 
-      def spaceship_params
-        params.require(:spaceship).permit(:name, :category, :location, :price, :description, :speed_of_light, :capacity, :fuel_drive, :brand)
-      end
+  private
+
+  def spaceship_params
+    params.require(:spaceship).permit(:name, :category, :location, :price, :description, :speed_of_light, :capacity, :fuel_drive, :brand)
+  end
 end
